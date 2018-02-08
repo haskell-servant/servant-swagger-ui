cbd19054a5735fab8a75862e159672780c7c6075
Cliff Harvey <cs.hbar@gmail.com>
2018-02-08 14:59:00 +0100

    Update to Swagger UI 3.9.1 (#40)
    Update to Swagger UI 3.9.1

--- src/Servant/Swagger/UI.hs
+++ src/Servant/Swagger/UI.hs
@@ -177,7 +177,7 @@ swaggerUiIndexTemplate :: T.Text
 swaggerUiIndexTemplate = $(embedStringFile "index.html.tmpl")
 
 swaggerUiFiles :: [(FilePath, ByteString)]
-swaggerUiFiles = $(mkRecursiveEmbedded "swagger-ui-dist-3.4.0")
+swaggerUiFiles = $(mkRecursiveEmbedded "swagger-ui-dist-3.9.1")
 
 jensolegIndexTemplate :: T.Text
 jensolegIndexTemplate = $(embedStringFile "jensoleg.index.html.tmpl")
