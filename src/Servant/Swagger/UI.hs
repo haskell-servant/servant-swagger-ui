d87067bb2cf01fa0ab7b28369f6f7b23097ff54e
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-04-05 11:08:59 +0300

    Use file-embed-lzma

--- src/Servant/Swagger/UI.hs
+++ src/Servant/Swagger/UI.hs
@@ -71,11 +71,11 @@ module Servant.Swagger.UI (
 
 import Data.ByteString                (ByteString)
 import Data.Swagger                   (Swagger)
+import FileEmbedLzma
 import GHC.TypeLits                   (KnownSymbol, Symbol, symbolVal)
 import Network.Wai.Application.Static (embeddedSettings, staticApp)
 import Servant
 import Servant.HTML.Blaze             (HTML)
-import Servant.Swagger.UI.Internal
 import Text.Blaze                     (ToMarkup (..))
 
 import qualified Data.Text as T
@@ -192,16 +192,16 @@ swaggerUiIndexTemplate :: T.Text
 swaggerUiIndexTemplate = $(embedText "index.html.tmpl")
 
 swaggerUiFiles :: [(FilePath, ByteString)]
-swaggerUiFiles = $(mkRecursiveEmbedded "swagger-ui-dist-3.9.1")
+swaggerUiFiles = $(embedRecursiveDir "swagger-ui-dist-3.9.1")
 
 jensolegIndexTemplate :: T.Text
 jensolegIndexTemplate = $(embedText "jensoleg.index.html.tmpl")
 
 jensolegFiles :: [(FilePath, ByteString)]
-jensolegFiles = $(mkRecursiveEmbedded "jensoleg-dist")
+jensolegFiles = $(embedRecursiveDir "jensoleg-dist")
 
 redocIndexTemplate :: T.Text
-redocIndexTemplate = $(embedStringFile "redoc.index.html.tmpl")
+redocIndexTemplate = $(embedText "redoc.index.html.tmpl")
 
 redocFiles :: [(FilePath, ByteString)]
-redocFiles = $(mkRecursiveEmbedded "redoc-dist")
+redocFiles = $(embedRecursiveDir "redoc-dist")
